e5807fdd8ea1931924c37ab0de9236897853ead5
TotskayaOV <olga.totskaya@gmail.com>
2023-04-20 23:05:05 +0700

    удален мусор2

--- excel_worker/work_with_file.py
+++ excel_worker/work_with_file.py
@@ -4,9 +4,6 @@ import xlsxwriter
 def uploading_file(data):
     workbook = xlsxwriter.Workbook('temp_dir/pivot.xlsx')
     worksheet = workbook.add_worksheet()
-    #id INTEGER PRIMARY KEY AUTOINCREMENT,
-        # agent_name TEXT, phone_number INTEGER, inn_number INTEGER,
-        # company_name TEXT, date_up DATETIME, date_down DATETIME, comment TEXT, last_user INTEGER
     for i in range(len(data)):
         for y in range(len(data[i])):
             worksheet.write(i, y, data[i][y])
