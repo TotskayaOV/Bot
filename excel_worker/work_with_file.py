f0afca0cbe14ed216bac3558bb18779cfcc28cb8
TotskayaOV <olga.totskaya@gmail.com>
2023-04-20 23:04:42 +0700

    удален мусор

--- excel_worker/work_with_file.py
+++ excel_worker/work_with_file.py
@@ -1,15 +1,5 @@
 import xlsxwriter
-from loader import db
-# открываем новый файл на запись
-workbook = xlsxwriter.Workbook('hello.xlsx')
-# создаем там "лист"
-worksheet = workbook.add_worksheet()
-# в ячейку A1 пишем текст
-worksheet.write('A1', 'Hello world')
-# сохраняем и закрываем
-workbook.close()
-worksheet.write(0, 0, 'Это A1!')
-worksheet.write(4, 3, 'Колонка D, стока 5')
+
 
 def uploading_file(data):
     workbook = xlsxwriter.Workbook('temp_dir/pivot.xlsx')
